945ac557e982c2d9af1735042b465ae61ba4f4fe
Marinus Damm <github@plesba.com>
2018-05-13 10:14:18 -0600

    Progress on transformations.

--- python/RecordValues.py
+++ python/RecordValues.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+#  version 2018-05-13
+
 from datetime import datetime
 import time
 import serial
@@ -11,12 +13,12 @@ tuning = True
 now = datetime.now()
 file_path = '/home/pi/datalogs/' + now.strftime('%Y-%m-%d.%H:%M') + '.csv'
 nano = 0 # this will be a serial connection
-micros_per_minute = 1000000
+micros_per_minute = 1000000  # microseconds
 analog_factor = 0.0048828125  # 0 = 0V, 512 = 2.5V, 1024 = 5V
 
 #####FUNCTIONS#############################################
 #initialize serial connection
-def init_serial():
+def init_nano():
     global nano
     # baud must match what's in the Arduino sketch
     nano = serial.Serial('/dev/serial0', 38400, timeout = 1)
@@ -30,13 +32,13 @@ def init_serial():
 def init_gps():
     gpsd.connect()
     packet = gpsd.get_current()
-    if (packet.mode > 1):   # we have either a 2D or 3D fix
+    if (packet.mode > 1):   # then we have either a 2D or 3D fix
     # See the inline docs for GpsResponse for the available data
         print('GPS: ' + str(packet.position()))
     else:
         print('GPS: no position fix from device: ' + str(gpsd.device()))
 
-def get_gps():
+def get_gps_data():
     lat = lon = alt = mph = utc = '' # make them empty strings
     packet = gpsd.get_current()
     if (packet.mode >= 2):
@@ -55,6 +57,27 @@ def get_axle_rpm(micros,count):
         pulses_per_minute = micros_per_minute / pulse_time_micros
     return str(int(pulses_per_minute))
 
+# linear potentiometers give a voltage between 0V-3.3V;
+#   arduino encodes to an int 0-1024; and we want a range 0..100
+# The ride height sensor readings go DOWN as the accordion units are extended
+#  and the readings go UP as the units are compressed, so we must invert.
+def get_ride_height(pinValue):
+    return 100 - (pinValue * 100 / 1024)
+
+# VDO 10-bar pressure sensor has resistance between 8ohms and 180ohms
+#  A voltage divider circuit gives between about 1.6V and 4.8V to the arduino
+#  arduino encodes to an int 0-1024; we will not see the min/max values
+#  when pressure is high, voltage is low; so invert (and normalize to 0-100)
+def get_fuel_pressure(pinValue):
+    return 100 - (pinValue * 100 / 1024)
+
+# VDO 10-bar pressure sensor has resistance between 8ohms and 180ohms
+#  A voltage divider circuit gives between about 1.6V and 4.8V to the arduino
+#  arduino encodes to an int 0-1024; we will not see the min/max values
+#  when pressure is high, voltage is low; so invert
+def get_fuel_temperature(pinValue):
+    return 1024 - pinValue
+
 # the NTK gives a voltage from 0V-5V; the arduino turns that into a int between 0-1024
 def get_afr(pinValue):
     voltage = pinValue * analog_factor  # convert from 10bits to voltage
@@ -62,7 +85,7 @@ def get_afr(pinValue):
     (whole,fraction) = str(afr).split('.')
     return whole + '.' + fraction[:1]  # return one fractional digit
 
-def get_nano():
+def get_nano_data():
     global nano
     nano.write(str('d').encode())
     nano_data = nano.readline().decode('ascii').rstrip()
@@ -72,13 +95,31 @@ def get_nano():
     rawLeftRideHeight,rawRightRideHeight,
     rawFuelPressure,rawFuelTemperature,
     rawGearPosition,rawAirFuelRatio,
-    rawManifoldAbsolutePressure,rawExhaustGasTemperature) = nano_data.split(',')
+    rawManifoldAbsolutePressure,rawExhaustGasTemperature
+    ) = nano_data.split(',')
 
     front_rpm = get_axle_rpm(deltaFrontCount,deltaFrontMicros)
     rear_rpm = get_axle_rpm(deltaRearCount,deltaRearMicros)
+    lrh = get_ride_height(rawLeftRideHeight)
+    rrh = get_ride_height(rawRightRideHeight)
+    fp = get_fuel_pressure(rawFuelPressure)
+    ft = get_fuel_temperature(rawFuelTemperature)
+    gp = get_gear_position(rawGearPosition)
     afr = get_afr(rawAirFuelRatio)
+    man = get_man_abs_pressure(rawManifoldAbsolutePressure)
+    egt = get_egt(rawExhaustGasTemperature)
 
-    return lat + ',' + lon + ',' + alt + ',' + mph + ',' + utc
+    return (front_rpm + ','
+             rear_rpm + ','
+                  lrh + ','
+                  rrh + ','
+                   ft + ','
+                   fp + ','
+                   gp + ','
+                  afr + ','
+                  map + ','
+                  egt
+            )
 
 def write_header():
     global nano
@@ -88,8 +129,8 @@ def write_header():
     gps_header = 'latitude,longitude,altitudeFt,mph,utc'
     output_file.write(nano_header + ',' + gps_header + '\n')
 
-#####################################################
-init_serial()
+##### MAIN MAIN MAIN ###################################
+init_nano()
 init_gps()
 
 output_file = open(file_path,'w')
@@ -102,9 +143,9 @@ try:
     while True:
         if (i % 20 == 0 and tuning):
             write_header()
-        nano.write(str('d').encode())
+        nano.write(str('d').encode())  # command nano to send data
         nano_data = nano.readline().decode('ascii').rstrip()
-        gps_data = get_gps()
+        gps_data = get_gps_data()
         output_file.write(nano_data + ',' + gps_data + '\n')
         time.sleep(0.33) # 3Hz max
         i =+ 1
