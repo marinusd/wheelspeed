f0f53580c516a4e1ba4726aa0ed04370d1483306
Marinus Damm <github@plesba.com>
2018-05-16 07:04:49 -0600

    Pull the calcs out into an accessory file.

---
+++ python/Decode.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python3
+
+from datetime import datetime
+import time
+import gpsd    #  pip3 install gpsd-py3 https://github.com/MartijnBraam/gpsd-py3
+
+# constants
+micros_per_minute = 1000000  # microseconds
+analog_factor = 0.0048828125  # 0 = 0V, 512 = 2.5V, 1024 = 5V
+gps_header = 'latitude,longitude,altitudeFt,mph,utc'
+readings_header = 'time,mph,fRpm,rRpm,afr,map,fTemp,fPress,lrh,rrh,utc'
+
+def get_gps_data():
+    lat = lon = alt = mph = utc = '' # make them empty strings
+    try:
+        packet = gpsd.get_current()
+        if (packet.mode >= 2):
+            lat = str(packet.lat)
+            lon = str(packet.lon)
+            mph = str(int(packet.hspeed * 2.2369363)) # speed in m/s, we use mph
+            utc = str(packet.time)
+        if (packet.mode >= 3):
+            alt = str(int(packet.alt * 3.2808399)) # alt in meters, we use feet
+    except:
+        print("Exception in get_gps_data")
+    return lat + ',' + lon + ',' + alt + ',' + mph + ',' + utc
+
+def get_axle_rpm(micros,count):
+    pulses_per_minute = 0
+    if count > 0:
+        pulse_time_micros = (micros / count)  # each pulse arrived, on average
+        pulses_per_minute = micros_per_minute / pulse_time_micros
+    return str(int(pulses_per_minute))
+
+# linear potentiometers give a voltage between 0V-3.3V;
+#   arduino encodes to an int 0-1024; and we want a range 0..100
+# The ride height sensor readings go DOWN as the accordion units are extended
+#  and the readings go UP as the units are compressed, so we must invert.
+def get_ride_height(pinValue):
+    return str(int(100 - (pinValue * 100 / 1024)))
+
+# VDO 10-bar pressure sensor has resistance between 8ohms and 180ohms
+#  A voltage divider circuit gives between about 1.6V and 4.8V to the arduino
+#  arduino encodes to an int 0-1024; we will not see the min/max values
+#  when pressure is high, voltage is low; so invert (and normalize to 0-100)
+def get_fuel_pressure(pinValue):
+    return str(int(100 - (pinValue * 100 / 1024)))
+
+# temp sensor resistance is between 3200 and 12 ohms
+#  Resistance decreases with increasing temperature
+#  The voltage divider circuit converts the resistance to a voltage between about
+#   1.6V (low) and 4.9V (high)
+#  The arduino will report that voltage as an int between 0-1024
+def get_fuel_temperature(pinValue):
+    return str(int(pinValue * 100 / 1024))
+
+# the NTK gives a voltage from 0V-5V; the arduino turns that into a int between 0-1024
+def get_afr(pinValue):
+    voltage = pinValue * analog_factor  # convert from 10bits to voltage
+    afr = 9 + (1.4 * voltage)   # according to the NTK doc, 0V = 9.0, 5V = 16.0
+    (whole,fraction) = str(afr).split('.')
+    return whole + '.' + fraction[:1]  # return one fractional digit
+
+# the MAP gives a voltage from 0V-5V; the arduino turns that into a int between 0-1024
+def get_map(pinValue):
+    voltage = pinValue * analog_factor  # convert from 10bits to voltage
+    # 11kPa = 0.25V, 307kPa = 4.75V, so
+    kpa = voltage * 64.6315789473
+    psi = kpa * 0.14503774
+    (whole,fraction) = str(psi).split('.')
+    return whole + '.' + fraction[:1]  # return one fractional digit
+
+def get_readings(raw_nano_data,gps_data):
+    mph = fRpm = rRpm = afr = man = ft = fp = lrh = rrh = utc = ''
+    # cook the nano data
+    try:
+        (millis,
+        frontCount,deltaFrontCount,deltaFrontMicros,
+        rearCount,deltaRearCount,deltaRearMicros,
+        rawLeftRideHeight,rawRightRideHeight,
+        rawFuelPressure,rawFuelTemperature,
+        rawGearPosition,rawAirFuelRatio,
+        rawManifoldAbsolutePressure,rawExhaustGasTemperature
+        ) = raw_nano_data.split(',')
+        (lat,lon,alt,mph,utc) = gps_data.split(',')
+        # calcs and transforms
+        fRpm = get_axle_rpm(deltaFrontCount,deltaFrontMicros)
+        rRpm = get_axle_rpm(deltaRearCount,deltaRearMicros)
+        afr = get_afr(rawAirFuelRatio)
+        man = get_man_abs_pressure(rawManifoldAbsolutePressure)
+        ft  = get_fuel_temperature(rawFuelTemperature)
+        fp  = get_fuel_pressure(rawFuelPressure)
+        lrh = get_ride_height(rawLeftRideHeight)
+        rrh = get_ride_height(rawRightRideHeight)
+        #gp  = get_gear_position(rawGearPosition)
+        #egt = get_egt(rawExhaustGasTemperature)
+    except:
+        print("exception in get_readings")
+    # returnCols = 'mph,fRpm,rRpm,afr,map,ftemp,fpress,lrh,rrh,utc'
+    return ( mph + ',' +
+            fRpm + ',' +
+            rRpm + ',' +
+             afr + ',' +
+             man + ',' +
+              ft + ',' +
+              fp + ',' +
+             lrh + ',' +
+             rrh + ',' +
+             utc
+            )
--- python/RecordValues.py
+++ python/RecordValues.py
@@ -8,9 +8,10 @@ from datetime import datetime
 import time
 import serial  #  pip3 install pyserial
 import gpsd    #  pip3 install gpsd-py3 https://github.com/MartijnBraam/gpsd-py3
+import Decode
 
 # constants
-data_dir = '/home/pi/datalogs'
+data_dir = '/tmp/pi/datalogs'
 file_timestamp = datetime.now().strftime('%Y-%m-%d.%H:%M')
 raw_log_file_path = data_dir + '/raw-' + file_timestamp + '.csv'
 data_file_path = data_dir + '/data-' + file_timestamp + '.csv'
@@ -71,105 +72,6 @@ def get_raw_nano_data():
         print("exception in get_raw_nano_data")
     return raw_nano_data
 
-def get_gps_data():
-    lat = lon = alt = mph = utc = '' # make them empty strings
-    try:
-        packet = gpsd.get_current()
-        if (packet.mode >= 2):
-            lat = str(packet.lat)
-            lon = str(packet.lon)
-            mph = str(int(packet.hspeed * 2.2369363)) # speed in m/s, we use mph
-            utc = str(packet.time)
-        if (packet.mode >= 3):
-            alt = str(int(packet.alt * 3.2808399)) # alt in meters, we use feet
-    except:
-        print("Exception in get_gps_data")
-    return lat + ',' + lon + ',' + alt + ',' + mph + ',' + utc
-
-def get_axle_rpm(micros,count):
-    pulses_per_minute = 0
-    if count > 0:
-        pulse_time_micros = (micros / count)  # each pulse arrived, on average
-        pulses_per_minute = micros_per_minute / pulse_time_micros
-    return str(int(pulses_per_minute))
-
-# linear potentiometers give a voltage between 0V-3.3V;
-#   arduino encodes to an int 0-1024; and we want a range 0..100
-# The ride height sensor readings go DOWN as the accordion units are extended
-#  and the readings go UP as the units are compressed, so we must invert.
-def get_ride_height(pinValue):
-    return str(int(100 - (pinValue * 100 / 1024)))
-
-# VDO 10-bar pressure sensor has resistance between 8ohms and 180ohms
-#  A voltage divider circuit gives between about 1.6V and 4.8V to the arduino
-#  arduino encodes to an int 0-1024; we will not see the min/max values
-#  when pressure is high, voltage is low; so invert (and normalize to 0-100)
-def get_fuel_pressure(pinValue):
-    return str(int(100 - (pinValue * 100 / 1024)))
-
-# temp sensor resistance is between 3200 and 12 ohms
-#  Resistance decreases with increasing temperature
-#  The voltage divider circuit converts the resistance to a voltage between about
-#   1.6V (low) and 4.9V (high)
-#  The arduino will report that voltage as an int between 0-1024
-def get_fuel_temperature(pinValue):
-    return str(int(pinValue * 100 / 1024))
-
-# the NTK gives a voltage from 0V-5V; the arduino turns that into a int between 0-1024
-def get_afr(pinValue):
-    voltage = pinValue * analog_factor  # convert from 10bits to voltage
-    afr = 9 + (1.4 * voltage)   # according to the NTK doc, 0V = 9.0, 5V = 16.0
-    (whole,fraction) = str(afr).split('.')
-    return whole + '.' + fraction[:1]  # return one fractional digit
-
-# the MAP gives a voltage from 0V-5V; the arduino turns that into a int between 0-1024
-def get_map(pinValue):
-    voltage = pinValue * analog_factor  # convert from 10bits to voltage
-    # 11kPa = 0.25V, 307kPa = 4.75V, so
-    kpa = voltage * 64.6315789473
-    psi = kpa * 0.14503774
-    (whole,fraction) = str(psi).split('.')
-    return whole + '.' + fraction[:1]  # return one fractional digit
-
-def get_readings(raw_nano_data,gps_data):
-    mph = fRpm = rRpm = afr = man = ft = fp = lrh = rrh = utc = ''
-    # cook the nano data
-    try:
-        (millis,
-        frontCount,deltaFrontCount,deltaFrontMicros,
-        rearCount,deltaRearCount,deltaRearMicros,
-        rawLeftRideHeight,rawRightRideHeight,
-        rawFuelPressure,rawFuelTemperature,
-        rawGearPosition,rawAirFuelRatio,
-        rawManifoldAbsolutePressure,rawExhaustGasTemperature
-        ) = raw_nano_data.split(',')
-        (lat,lon,alt,mph,utc) = gps_data.split(',')
-        # calcs and transforms
-        fRpm = get_axle_rpm(deltaFrontCount,deltaFrontMicros)
-        rRpm = get_axle_rpm(deltaRearCount,deltaRearMicros)
-        afr = get_afr(rawAirFuelRatio)
-        man = get_man_abs_pressure(rawManifoldAbsolutePressure)
-        ft  = get_fuel_temperature(rawFuelTemperature)
-        fp  = get_fuel_pressure(rawFuelPressure)
-        lrh = get_ride_height(rawLeftRideHeight)
-        rrh = get_ride_height(rawRightRideHeight)
-        #gp  = get_gear_position(rawGearPosition)
-        #egt = get_egt(rawExhaustGasTemperature)
-    except:
-        print("exception in get_readings")
-    # returnCols = 'mph,fRpm,rRpm,afr,map,ftemp,fpress,lrh,rrh,utc'
-    return ( mph + ',' +
-            fRpm + ',' +
-            rRpm + ',' +
-             afr + ',' +
-             man + ',' +
-              ft + ',' +
-              fp + ',' +
-             lrh + ',' +
-             rrh + ',' +
-             utc
-            )
-
 def write_raw_log_header():
     global NANO
     global RAW_LOG_FILE
@@ -190,12 +92,12 @@ def write_data_file(timestamp,readings):
     DATA_FILE.write(timestamp + ',' + readings + '\n')
 
 ##### MAIN MAIN MAIN ###################################
-init_gps()
-init_nano()
+#init_gps()
+#init_nano()
 
 RAW_LOG_FILE = open(raw_log_file_path,'w')
 print('Writing raw log to ' + raw_log_file_path)
-write_raw_log_header()
+#write_raw_log_header()
 
 DATA_FILE = open(data_file_path,'w')
 print('Writing data to ' + data_file_path)
@@ -207,11 +109,11 @@ while True:
         time.sleep(0.33) # 3Hz max
         # example timestamp: 1526430861.829
         timestamp = datetime.now().strftime('%s.%f')[:-3]
-        gps_data = get_gps_data()
+        gps_data = Decode.get_gps_data()
         raw_nano_data = get_raw_nano_data()
         write_raw_log(timestamp, raw_nano_data, gps_data)
         # now the processed numbers
-        readings = get_readings(raw_nano_data, gps_data)
+        readings = Decode.get_readings(raw_nano_data, gps_data)
         write_data_file(timestamp, readings)
     except KeyboardInterrupt:
         print("\nShutting down")
