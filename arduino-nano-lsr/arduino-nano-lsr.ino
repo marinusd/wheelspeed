f3388e396a157619d71704b87fdc2d7e3e9030ad
Marinus Damm <marinus.damm@plesba.com>
2018-06-16 16:42:23 -0600

    Update version, rename function to avoid built-in, add resetCount

--- arduino-nano-lsr/arduino-nano-lsr.ino
+++ arduino-nano-lsr/arduino-nano-lsr.ino
@@ -1,5 +1,5 @@
 // Gen IV module sketch
-const long Version = 20180521;  // 2018-05-21
+const long Version = 20180616;  // 2018-06-16
 const char subVersion = 'a';
 
 // Digital Pins
@@ -8,7 +8,7 @@ const char subVersion = 'a';
 // Pin 13 drives the on-board user LED.
 // No other digital pins are used.
 
-// Wheel Speed sensors are Hall Effect. The pulses trigger interrupts because we'd like to count every pulse. 
+// Wheel Speed sensors are Hall Effect. The pulses trigger interrupts because we'd like to count every pulse.
 // To get rotation speed, we gather rotation count and elapsed time between readings in the ISR.
 //  n.b. volatiles force loading from memory not registers. Required for interrupts.
 volatile unsigned int  frontCount,  rearCount  = 0u;
@@ -29,11 +29,11 @@ const int ledPin =  LED_BUILTIN; // fetches the number of the LED pin
 //  analogRead() returns ints between 0-1023
 
 // rideHeightSensors - analog, 0V-3.3V return
-int rawLeftRideHeight, rawRightRideHeight = 0;  
+int rawLeftRideHeight, rawRightRideHeight = 0;
 const int pinRightRideHeight = A0; // small diameter white
 const int pinLeftRideHeight  = A1; // small diameter green
 
-// 0V-5V input pins 
+// 0V-5V input pins
 int rawAirFuelRatio, rawManifoldAbsolutePressure;
 const int pinManifoldAbsolutePressure = A2; //grey
 const int pinAirFuelRatio             = A3; // large green
@@ -46,16 +46,16 @@ const int pinFuelTemperature = A6; // blue
 
 // unknowns
 int rawExhaustGasTemperature = 0;
-const int pinExhaustGasTemperature = A7; // small diameter white 
+const int pinExhaustGasTemperature = A7; // small diameter white
 
-// Serial (UART) commands 
+// Serial (UART) commands
 int incomingByte = 0;
-char command = 'z';  // unassigned char
+char command = 'u';  // unassigned char
 
 // Get all the things lined up
 void setup ()
 {
-  Serial.begin(38400);  
+  Serial.begin(38400);
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
@@ -70,14 +70,14 @@ void setup ()
   // will be the only sign something is working; serial through the UART does not blink the lights
   pinMode(ledPin, OUTPUT);
 
-  // pullup the unused digital pins 
-  // 0&1 are serial, 2&3 are wheel sensor 
+  // pullup the unused digital pins
+  // 0&1 are serial, 2&3 are wheel sensor
   pinMode(4, INPUT_PULLUP);
   pinMode(5, INPUT_PULLUP);
   pinMode(6, INPUT_PULLUP);
   pinMode(7, INPUT_PULLUP);
   pinMode(8, INPUT_PULLUP);
-  pinMode(9, INPUT_PULLUP);  
+  pinMode(9, INPUT_PULLUP);
   pinMode(10, INPUT_PULLUP);
   pinMode(11, INPUT_PULLUP);
   pinMode(12, INPUT_PULLUP);
@@ -99,6 +99,15 @@ void rearPulseISR ()
   rearMicros = micros();
 }
 
+void resetCounts() {
+  // zero  wheel sensor data
+  noInterrupts ();  // don't allow changes while we read the volatiles
+  frontCount = 0;
+  rearCount  = 0;
+  interrupts (); // interrupts on again
+  Serial.println("Wheel counts reset");
+}
+
 // for tracking wheel speed
 void updateDeltaWheelValues() {
   deltaFrontCount  = nowFrontCount  - prevFrontCount;
@@ -112,33 +121,33 @@ void updateDeltaWheelValues() {
   prevRearMicros  = nowRearMicros;
 }
 
-// this is the main data collection method. 
+// this is the main data collection method.
 void collectReadings() {
-  // get wheel sensor data 
-  noInterrupts ();  // dont allow changes while we read the volatiles
+  // get wheel sensor data
+  noInterrupts ();  // don't allow changes while we read the volatiles
   nowFrontCount  = frontCount;
   nowFrontMicros = frontMicros;
   nowRearCount   = rearCount;
   nowRearMicros  = rearMicros;
   interrupts (); // interrupts on again
-  
+
   // gather the ADC inputs. The values returned are 0-1023 based on voltage.
-  rawRightRideHeight          = analogRead(pinRightRideHeight); 
-  rawLeftRideHeight           = analogRead(pinLeftRideHeight); 
+  rawRightRideHeight          = analogRead(pinRightRideHeight);
+  rawLeftRideHeight           = analogRead(pinLeftRideHeight);
   rawFuelPressure             = analogRead(pinFuelPressure);
   rawFuelTemperature          = analogRead(pinFuelTemperature);
   rawGearPosition             = analogRead(pinGearPosition);
   rawAirFuelRatio             = analogRead(pinAirFuelRatio);
   rawManifoldAbsolutePressure = analogRead(pinManifoldAbsolutePressure);
   rawExhaustGasTemperature    = analogRead(pinExhaustGasTemperature);
-  
-  // gotta do some calcs on the data we pulled 
-  updateDeltaWheelValues();   
+
+  // gotta do some calcs on the data we pulled
+  updateDeltaWheelValues();
 }
 
 // one of these functions will be asked for by the Linux box
-void printVersion() {
-  Serial.print("Version: "); Serial.print(Version); Serial.println(subVersion); 
+void printSketchVersion() {
+  Serial.print("Version: "); Serial.print(Version); Serial.println(subVersion);
 }
 void printHeader() {
   Serial.print("millis");                       Serial.print(',');
@@ -155,7 +164,7 @@ void printHeader() {
   Serial.print("rawGearPosition");              Serial.print(',');
   Serial.print("rawAirFuelRatio");              Serial.print(',');
   Serial.print("rawManifoldAbsolutePressure");  Serial.print(',');
-  Serial.println("rawExhaustGasTemperature"); 
+  Serial.println("rawExhaustGasTemperature");
 }
 void printOutput () {
   collectReadings();
@@ -173,15 +182,15 @@ void printOutput () {
   Serial.print(rawGearPosition);              Serial.print(',');
   Serial.print(rawAirFuelRatio);              Serial.print(',');
   Serial.print(rawManifoldAbsolutePressure);  Serial.print(',');
-  Serial.println(rawExhaustGasTemperature); 
+  Serial.println(rawExhaustGasTemperature);
 }
 
 // The main() event
 void loop ()
-{ 
-  // we check for an incoming command   
+{
+  // we check for an incoming command
   if (Serial.available() > 0) {
-    
+
     // throw away all but the last command byte
     while (Serial.available() > 0) {
       incomingByte = Serial.read();
@@ -190,7 +199,7 @@ void loop ()
       }
     }
     digitalWrite(ledPin, HIGH); // showing we're doing something
-    
+
     // we have a Byte - what is it?
     if (command == 'd') {
       printOutput();  // this also does the reads()
@@ -199,13 +208,16 @@ void loop ()
       printHeader();
 
     } else if (command == 'v') {
-      printVersion();
+      printSketchVersion();
+
+    } else if (command == 'z') {
+      resetCounts();
 
     } else {
-      Serial.print(" ? Send h for header, d for data, v for version. Received: "); Serial.println(command);
+      Serial.print(" ? Send h for header, d for data, v for version, z to zero wheel counts. Received: "); Serial.println(command);
     }
-    
-  } // the end of the if; we come here immediately when there's no incoming command 
+
+  } // the end of the if; we come here immediately when there's no incoming command
 
   delay(100); // pause a tenth of a second; a caller will wait on average 1/2 of that
   digitalWrite(ledPin, LOW); // this may already be off
