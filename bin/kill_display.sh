acc2da82ede96beeec9ed97915920e099d87b6db
Marinus Damm <marinus.damm@gmail.com>
2020-07-30 17:42:51 -0600

    Change second SIGKILL to SIGINT, seems to work.

--- bin/kill_display.sh
+++ bin/kill_display.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sudo kill -SIGINT $(</var/run/PickleDisplay.pid)
-sudo kill -SIGKILL $(</var/run/PickleDisplay.pid)
+sudo kill -SIGINT $(</var/run/PickleDisplay.pid)
 cat /var/log/PickleDisplay.log
 
