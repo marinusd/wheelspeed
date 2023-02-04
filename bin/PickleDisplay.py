21491a90f6edd904fbd39f8435fd4688acf6b53e
Marinus Damm <github.user@plesba.com>
2023-02-04 09:45:01 -0700

    Make button 4 the 'shutdown' button

--- bin/PickleDisplay.py
+++ bin/PickleDisplay.py
@@ -16,69 +16,58 @@ GREEN = (0, 200, 0)
 BLUE = (0, 0, 255)
 CYAN = (0, 255, 255)
 
-# hookup the display for output
-print('initing pygame display')
-os.putenv('SDL_FBDEV', '/dev/fb1')
-pygame.init()
-pygame.mouse.set_visible(False)
-font_30 = pygame.font.Font(None, 30)
-font_33 = pygame.font.Font(None, 33)
-font_36 = pygame.font.Font(None, 36)
-font_40 = pygame.font.Font(None, 40)
-font_50 = pygame.font.Font(None, 50)
-font_60 = pygame.font.Font(None, 60)
-lcd = pygame.display.set_mode((320, 240))
-lcd.fill(BLACK)
-pygame.display.update()
-print('pygame display set')
+# Buttons (GPIO pins)
+WIFI = 17
+RECORDER = 22
+LIVEREAD = 23
+SHUTDOWN = 27
 
-# start out with reading disabled
-get_live_reading = False
-# try to be independent of the PickleRecorder, just tail its output file.
-def fetch_reading():
-  readings_map = {}
-  try:
-    raw_data_line = subprocess.check_output('tail -1 /var/www/html/data/current', shell=True)
-    (mph,fRpm,rRpm,afr,map,ftemp,fpress,lrh,rrh,utc) = Decoder.get_readings(raw_data_line).split(',')
-    readings_map = {'GPS MPH':mph,
-	'Front Wheel RPM':fRpm,
-	'Rear Wheel RPM':rRpm,
-	'A/F Ratio':afr,
-	'MAP':map,
-	'Fuel Pressure':fpress,
-	'Fuel Temp':ftemp
-	}
-  except Exception as e: # might be anything...
-    print('Error in fetch_reading: ' + str(e))
-  return readings_map
+# BUTTON INPUTS
+button_map = {
+    WIFI: {    'color': BLUE, 'text': 'Restarting WiFi',     'os_cmd': 'sudo service hostapd restart'},
+    RECORDER: {'color': GREY, 'text': 'Restarting Recorder', 'os_cmd': '/var/www/bin/kill_recorder.sh'},
+    LIVEREAD: {'color': GREEN,'text': 'Toggling Live Read',  'action': 'toggle'},
+    SHUTDOWN: {'color': RED,  'text': 'Shutdown; wait 10s',  'os_cmd': 'sleep 3; /var/www/bin/screen_off_and_shutdown.sh'}
+}
+options_map = {  # key is the vertical position
+    40: '    Restart WiFi ->',
+    100: 'Restart Recorder ->',
+    160: 'Toggle Live Read ->',
+    220: 'Shutdown picklePi->'
+}
+
 
 def ctl_reading(action):  # 'toggle' is the only action value
-  global get_live_reading
-  try:
-    if get_live_reading:
-      os.system('rm -f /var/www/html/data/live_readings')
-      get_live_reading = False
-    else:
-      os.system('touch /var/www/html/data/live_readings')
-      get_live_reading = True
-  except:
-    print('ctl_reading: error... of some sort')
+    global get_live_reading
+    try:
+        if get_live_reading:
+            os.system('rm -f /var/www/html/data/live_readings')
+            get_live_reading = False
+        else:
+            os.system('touch /var/www/html/data/live_readings')
+            get_live_reading = True
+    except:
+        print('ctl_reading: error... of some sort')
+
 
 def paint_row(text, row_center):
     text_surface = font_36.render(text, True, BLACK)
     rect = text_surface.get_rect(center=(160, row_center))
     lcd.blit(text_surface, rect)
 
+
 def paint_row_with_font(text, font, row_center):
     text_surface = font.render(text, True, BLACK)
     rect = text_surface.get_rect(center=(160, row_center))
     lcd.blit(text_surface, rect)
 
+
 # try to be independent of the PickleRecorder, just tail its output file.
 def show_live_reading():
     try:
         # grab the voltages, etc.
-        raw_data_line = subprocess.check_output('tail -1 /var/www/html/data/current', shell=True)
+        raw_data_line = subprocess.check_output(
+            'tail -1 /var/www/html/data/current', shell=True)
         # Decoder turns those into values we can use
         (mph, fRpm, rRpm, afr, man, ftemp, fpress, lrh, rrh, utc, rpm, egt1,
          egt2, egt3, egt4) = Decoder.get_readings(raw_data_line).split(',')
@@ -88,24 +77,25 @@ def show_live_reading():
         row_increment = 36
         row_center = row_increment  # the offset from the top of the display
 
-        row1 = ' MPH: '  + str(mph)  + '    RPM: '  + str(rpm)    + ' '
-        row2 = ' AFR: '  + str(afr)  + '    MAP: '  + str(man)    + ' '
-        row3 = ' FWHL: ' + str(fRpm) + '    RWHL: ' + str(rRpm)   + ' '
-        row4 = ' FTMP: ' + str(ftemp)+ '    FPRS: ' + str(fpress) + ' '
+        row1 = ' MPH: ' + str(mph) + '    RPM: ' + str(rpm) + ' '
+        row2 = ' AFR: ' + str(afr) + '    MAP: ' + str(man) + ' '
+        row3 = ' FWHL: ' + str(fRpm) + '    RWHL: ' + str(rRpm) + ' '
+        row4 = ' FTMP: ' + str(ftemp) + '    FPRS: ' + str(fpress) + ' '
         row5 = ' EGT1  EGT2  EGT3  EGT4 '
-        row6 = ' ' + str(egt1) + '    ' + str(egt2) + '    ' + str(egt3) + '    ' + str(egt4) + ' '
+        row6 = ' ' + str(egt1) + '    ' + str(egt2) + '    ' + \
+            str(egt3) + '    ' + str(egt4) + ' '
 
-        paint_row(row1,row_center)
+        paint_row(row1, row_center)
         row_center = row_center + row_increment
-        paint_row(row2,row_center)
+        paint_row(row2, row_center)
         row_center = row_center + row_increment
-        paint_row(row3,row_center)
+        paint_row(row3, row_center)
         row_center = row_center + row_increment
-        paint_row(row4,row_center)
+        paint_row(row4, row_center)
         row_center = row_center + row_increment + 2
-        paint_row_with_font(row5,font_33,row_center)
+        paint_row_with_font(row5, font_33, row_center)
         row_center = row_center + row_increment - 4
-        paint_row(row6,row_center)
+        paint_row(row6, row_center)
 
         lcd.blit(pygame.transform.rotate(lcd, 180), (0, 0))
         pygame.display.update()
@@ -125,22 +115,23 @@ def show_options():
     pygame.display.update()
 
 
-# BUTTON INPUT
-button_map = {
-  17:{'color':BLUE, 'text':'Restarting WiFi',      'os_cmd':'sudo service hostapd restart'},
-  22:{'color':GREY, 'text':'Restarting Recorder',  'os_cmd':'/var/www/bin/kill_recorder.sh'},
-  23:{'color':GREEN,'text':'Toggling Live Read',   'action':'toggle'},
-  27:{'color':RED,  'text':'Shutting Down Now',    'os_cmd':'sudo shutdown -h now'}
-} 
-options_map = { # key is the vertical position
-  40:'    Restart WiFi ->',
- 100:'Restart Recorder ->',
- 160:'Toggle Live Read ->',
- 220:'Shutdown picklePi->'
-}
-
-
 # # # MAIN # # #
+# hookup the display for output
+print('initing pygame display')
+os.putenv('SDL_FBDEV', '/dev/fb1')
+pygame.init()
+pygame.mouse.set_visible(False)
+font_30 = pygame.font.Font(None, 30)
+font_33 = pygame.font.Font(None, 33)
+font_36 = pygame.font.Font(None, 36)
+font_40 = pygame.font.Font(None, 40)
+font_50 = pygame.font.Font(None, 50)
+font_60 = pygame.font.Font(None, 60)
+lcd = pygame.display.set_mode((320, 240))
+lcd.fill(BLACK)
+pygame.display.update()
+print('pygame display set')
+
 # Setup the GPIOs as inputs with Pull Ups since the buttons are connected to GND
 GPIO.setmode(GPIO.BCM)
 print(GPIO.RPI_INFO)
@@ -155,37 +146,40 @@ show_options()
 
 # loop indefinitely
 while True:
-  try:
-    button_pressed = False
-    # Scan the buttons
-    for (BUTTON,DICT) in button_map.items():
-      if GPIO.input(BUTTON) == False:
-        button_pressed = True
-        lcd.fill(DICT['color'])
-        words = DICT['text']
-        text_surface = font_40.render('%s'%words, True, WHITE)
-        rect = text_surface.get_rect(center=(160,120))
-        lcd.blit(text_surface, rect)
-        lcd.blit(pygame.transform.rotate(lcd,180),(0,0))
-        pygame.display.update()
-        if 'os_cmd' in DICT:
-          os.system(DICT['os_cmd'])
-        else:
-          ctl_reading(DICT['action'])
-        sleep(1.1)
-    if not button_pressed:
-      if get_live_reading:
-        show_live_reading()
-        sleep(0.2)
-      else:
-        show_options()
-        sleep(0.2)
+    try:
+        button_pressed = False
+        # Scan the buttons
+        for (BUTTON, DICT) in button_map.items():
+            if GPIO.input(BUTTON) == False:  # when the button is down, its value is False
+                button_pressed = True
+                lcd.fill(DICT['color'])
+                words = DICT['text']
+                text_surface = font_40.render('%s' % words, True, WHITE)
+                rect = text_surface.get_rect(center=(160, 120))
+                lcd.blit(text_surface, rect)
+                lcd.blit(pygame.transform.rotate(lcd, 180), (0, 0))
+                pygame.display.update()
 
-  except KeyboardInterrupt:
-    print("Quitting on Ctrl-C")
-    break
-  except Exception as e:
-    print('Exception in display loop: ' + str(e))
+                if 'os_cmd' in DICT:
+                    os.system(DICT['os_cmd'])
+                    if BUTTON == SHUTDOWN:
+                        quit()  # keeps us from re-drawing the options screen
+                else:
+                    ctl_reading(DICT['action'])
+                sleep(1.1)
+
+        if not button_pressed:
+            if get_live_reading:
+                show_live_reading()
+            else:
+                show_options()
+            sleep(0.2)
+
+    except KeyboardInterrupt:
+        print("Quitting on Ctrl-C")
+        break
+    except Exception as e:
+        print('Exception in display loop: ' + str(e))
 
 # last act of a desperate program
 GPIO.cleanup()
---
+++ bin/screen_off_and_shutdown.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo  gpio -g mode 18 pwm
+sudo  shutdown -h now
